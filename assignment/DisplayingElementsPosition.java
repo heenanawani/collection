60c2ab67cf8d365a4efcc6ee2b7b510613ec5248
heenanawani <heenanawani13@gmail.com>
2020-01-23 11:05:55 +0530

    code updated

--- assignment/DisplayingElementsPosition.java
+++ assignment/DisplayingElementsPosition.java
@@ -1,5 +1,20 @@
 package assignment;
 
+import java.util.ArrayList;
+
 public class DisplayingElementsPosition {
 
+	public static void main(String[] args)
+	{
+		ArrayList<String> l=  new ArrayList();
+		l.add("Black");
+		l.add("Red");
+		l.add("White");
+		l.add("Blue");
+		  
+		for(int i=0; i<l.size();i++)
+		System.out.println(i + " "+ l.get(i));
+		
+	}
+	
 }
